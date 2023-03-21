b7b195089671704c16d5a19e827f343b32b2e5e8
cristianoans <cristianoans@hotmail.com>
2023-03-21 13:29:18 -0300

    comite inicial

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,11 @@
+export function Menu() {
+    return (
+        <div className="menu">
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/blog">Blog</a></li>
+                <li><a href="/contato">Contato</a></li>
+            </ul>
+        </div>
+    );
+};
