15c27d42e6fadeb22faee92a71df0f6fd24a3f4d
cristianoans <cristianoans@hotmail.com>
2023-03-21 13:48:46 -0300

    inserido o Routes e Rout, definido os links iniciais

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
+
 export function Menu() {
     return (
         <div className="menu">
             <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/blog">Blog</a></li>
-                <li><a href="/contato">Contato</a></li>
+                
+                <li>
+                <Link to='/'>Home</Link>
+                </li>
+                <li><Link to='/blog'>Blog</Link></li>
+                <li><Link to='/contato'>Contato</Link></li>
             </ul>
         </div>
     );
